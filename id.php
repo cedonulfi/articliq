8727caa54bfaa4af1b3ce80cc71229cc6238480d
Eko Sulistiyono <55071271+cedonulfi@users.noreply.github.com>
2024-11-07 17:07:06 +0700

    Create id.php
    Retrieves Author and Category IDs from your WordPress blog for assigning authorship and categorization.

---
+++ id.php
@@ -0,0 +1,24 @@
+<?php
+// Load WordPress environment
+require_once( dirname(__FILE__) . '/new/wp-load.php' );
+
+// Get list of authors
+$authors = get_users([
+    'who' => 'authors',
+]);
+
+echo "<h2>Author List</h2>";
+foreach ($authors as $author) {
+    echo 'ID: ' . $author->ID . ' - Name: ' . $author->display_name . '<br>';
+}
+
+// Get list of categories
+$categories = get_categories([
+    'hide_empty' => false,
+]);
+
+echo "<h2>Category List</h2>";
+foreach ($categories as $category) {
+    echo 'ID: ' . $category->term_id . ' - Name: ' . $category->name . '<br>';
+}
+?>
